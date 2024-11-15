55131f6b707feb7281cf3e236bfb59c6a06b44f2
Rana Moataz <ranamoataz02@gmail.com>
2024-11-15 17:25:34 +0200

    navbar finished

--- FrontEnd/employees-front/src/core/components/layout/nav/ui/NavBar.js
+++ FrontEnd/employees-front/src/core/components/layout/nav/ui/NavBar.js
@@ -1,6 +1,8 @@
 import NavData from "../data/NavData";
 import styles from "./navbar.module.css";
 import { Link } from "react-router-dom";
+import logo from "../../../../assets/imgs/kian_solutions_logo.jpg";
+import Hamburger from "../../../hamburger/Hamburger";
 
 const NavBar = ({ openmenu, setOpenmenu }) => {
   const navItems = NavData;
@@ -8,6 +10,7 @@ const NavBar = ({ openmenu, setOpenmenu }) => {
     <nav id="nav" className={styles.container}>
       <div className={styles.brandcontainer}>
         <Link to="/" onClick={() => setOpenmenu(false)}>
+          <img src={logo} alt="Kian Solutions Logo" className={styles.logo} />
         </Link>
       </div>
       <ul className={styles.menucontainer}>
@@ -21,6 +24,32 @@ const NavBar = ({ openmenu, setOpenmenu }) => {
             >
               <div className={styles.navitemtitle}>{element.title}</div>
             </Link>
+          );
+        })}
+      </ul>
+
+      <div className={styles.menuicon}>
+        <Hamburger isopen={openmenu} setopenmenu={setOpenmenu} />
+      </div>
+
+      <ul
+        className={`${styles.mobilenav} ${
+          openmenu ? styles.displaymobilenav : styles.hidemobilenav
+        }`}
+      >
+        {navItems.map((element, index) => {
+          return (
+            <Link
+              key={element.id}
+              className={styles.navitemlink}
+              scroll={true}
+              to={`${element.route}`}
+              onClick={() => setOpenmenu(false)}
+            >
+              <div className={styles.mobnavitemtitle}>{element.title}</div>
+            </Link>
+
+            
           );
         })}
       </ul>
